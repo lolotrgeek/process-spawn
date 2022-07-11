5d866e97766ec6bbd6ea19230f77585d4ac7a761
lolotrgeek <jon@simplyenable.com>
2022-07-11 14:47:12 -0500

    test: update to kill after started

--- tests/kill_test.js
+++ tests/kill_test.js
@@ -4,8 +4,10 @@ let file = dirname(require.main.filename) +"/testee.js"
 console.log(file)
 const spawner = new Spawner()
 spawner.spawn_node(file, 3, (message, node) => {
-    setTimeout(() => {
+    if (spawner.nodes.every(node => node.started === true)) setTimeout(() => {
         console.log("ending:", node.spawnargs[1])
         spawner.end_node(node)
+        
     },1000)
+    if(spawner.nodes.every(node => node.killed === true)) process.exit()
 })
