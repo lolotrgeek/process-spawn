0ab9a1c0f21f22bd897b4c6b13fb752c28a520e7
lolotrgeek <jon@simplyenable.com>
2022-07-13 08:40:46 -0500

    feat: update dashboard, removes table dep

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,31 +1,12 @@
 
-const { table } = require('table')
-
-let boards = []
-
-const col_config = {
-    drawVerticalLine: (lineIndex, columnCount) => {
-        return lineIndex === 0 || lineIndex === columnCount
-    }
-}
-
 function dashboard(message) {
-    if (Array.isArray(message)) {
-        let found = boards.findIndex(board => board[0][0][0] && board[0][0][0] === message[0][0][0])
-        if (found > -1) boards[found] = message
+    try {
+        let data = JSON.parse(message)
+        console.log(...data)
+    } catch (error) {
         console.log(message)
-        boards.push(message)
-        let dash = boards.map(board => [table(board[0], col_config), table(board[1], col_config), table(board[2], col_config)])
-        console.clear()
-        console.log(table(dash))
-    } else {
-        try {
-            let data = JSON.parse(message)
-            console.log(...data)
-        } catch (error) {
-            console.log(message)
-        }
     }
 }
+
 
 module.exports = { dashboard }
